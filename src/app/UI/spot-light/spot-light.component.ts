4bc49bfed51f174ba82efe8a9ea8c0647a5235e4
haythamIdrees9 <haythamidrees9@gmail.com>
2023-08-16 20:05:14 +0300

    focus on spto lite input on open

--- src/app/UI/spot-light/spot-light.component.ts
+++ src/app/UI/spot-light/spot-light.component.ts
@@ -1,16 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-spot-light[ariaLabel]',
   templateUrl: './spot-light.component.html',
   styleUrls: ['./spot-light.component.scss'],
 })
-export class SpotLightComponent {
+export class SpotLightComponent implements AfterViewInit {
+  @ViewChild('search_field') searchField!:ElementRef;
   @Input('ariaLabel') ariaLabel!:string;
   @Input('placeholder') placeholder!:string;
   @Output('onSearch') onSearch = new EventEmitter()
   enteredText:string = ""
   isSpotLightClosed:boolean = false;
+
+  ngAfterViewInit(): void {
+    this.searchField?.nativeElement?.focus();
+  }
+
   hideSpotLightInput(event:Event){
     event.stopPropagation();
     this.search("")

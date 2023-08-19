0bbdcf2b83709caad33e44e05dbb47c29ab1481d
haythamIdrees9 <haythamidrees9@gmail.com>
2023-08-19 14:52:28 +0300

    create raring  componen

---
+++ src/app/UI/rating/rating.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss'],
+  standalone:true,
+  imports:[CommonModule]
+})
+export class RatingComponent {
+  @Output('onRate') onRate = new EventEmitter();
+  hoveredPercent: number = 0;
+  isRateSubmitted: boolean = false;
+  rateItem(){
+    const starsRate = this.hoveredPercent / 20;    
+    this.onRate.emit(starsRate.toFixed(1));
+    this.isRateSubmitted = true;
+  }
+
+  calculateHoveredPercent(event: MouseEvent) {
+    if(this.isRateSubmitted){
+      return;
+    }
+    const container = event.target as HTMLElement;
+    const containerWidth = container.offsetWidth;
+    const mouseX = event.clientX - container.getBoundingClientRect().left;
+
+    this.hoveredPercent = (mouseX / containerWidth) * 100;
+  }
+
+  onMouseLeave() {
+    if(this.isRateSubmitted){
+      return;
+    }
+    this.hoveredPercent = 0;
+  }
+}

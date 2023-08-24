b891d131ea634f24404a7974f401e0ac9b4498d7
haythamIdrees9 <haythamidrees9@gmail.com>
2023-08-24 20:27:44 +0300

    implement touch on carousel

--- src/app/UI/carousel/carousel.component.ts
+++ src/app/UI/carousel/carousel.component.ts
@@ -24,6 +24,9 @@ export class CarouselComponent {
   debounceTime = 500;
   isWaitingAnimation = false;
   isTransitionEnabled = true;
+  touchX!:number;
+  touchY!:number;
+  isDragging = false;
   debounceUserClick() {
     if (!this.isButtonsDisabled) {
       this.isButtonsDisabled = true;
@@ -128,5 +131,44 @@ export class CarouselComponent {
       rect.right <= windowWidth
     );
   }
+
+  onTouchStart(event:TouchEvent){
+    this.touchX = event.touches[0].clientX;
+    this.touchY = event.touches[0].clientY;
+    this.isDragging = true;
+  }
+
+
+  onTouchMove(event:TouchEvent){
+    if (!this.isDragging){
+      return;
+    } 
+    const currentY = event.touches[0].clientY;
+    const deltaY = Math.abs(currentY - this.touchY);
+    if(deltaY > 50){
+      this.isDragging = false;
+    }
+
+    event.preventDefault();
+    const currentX = event.touches[0].clientX;
+    const deltaX = currentX - this.touchX;
+    console.log('deltaX',deltaX);
+    
+    if(deltaX > 50){
+      this.onTouchEnd();
+      this.previousItem();
+    } else if(deltaX < -50){
+      this.onTouchEnd();
+      this.nextItem();
+    } 
+  }
+
+  onTouchEnd(){
+    this.touchX = 0;
+    this.touchY = 0;
+    this.isDragging = false;
+  }
+
+  
   
 }
